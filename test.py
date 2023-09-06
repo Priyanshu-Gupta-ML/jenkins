c9051b9eeeda406467e1330ae916872288c3eaf7
Priyanshu-Gupta-ML <Priyanshu.Gupta@maplelabs.com>
2023-09-06 08:48:06 +0530

    source code change

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import json
+
+def lambda_handler(event, context):
+    response = {
+        "statusCode": 200,
+        "body": json.dumps("AWS Lambda and S3 Bucket - New version")
+    }
+    return response
