e30c5f8337e7aac171c8e0ba133af35119f5ec4a
Priyanshu-Gupta-ML <Priyanshu.Gupta@maplelabs.com>
2023-09-07 10:12:55 +0530

    lambda function changes

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    response = {
-        "statusCode": 200,
-        "body": json.dumps("AWS Lambda and S3 Bucket - New version")
+    return {
+        'statusCode': 200,
+        'body': json.dumps("AWS Lambda and S3 Bucket - New version")
     }
-    return response
